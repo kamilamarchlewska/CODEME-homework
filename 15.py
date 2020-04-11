74072012411a8dd6d72755a1783908fb90ef02dd
kamilamarchlewska <61703747+kamilamarchlewska@users.noreply.github.com>
2020-04-12 00:23:59 +0200

    Delete 15.py

--- 15.py
+++
@@ -1,9 +0,0 @@
-#Pobierz od użytkownika dowolny tekst i wyświetl tylko te znaki, które są na pozycjach parzystych.
-# Wykonaj na dwa sposoby - za pomocą pętli oraz przez sting slicing ( ‘abrakadabra’ -> ‘baaar’).
-
-magic = input('Proszę o podanie zaklęcia: ')
-
-magic = magic.replace(' ', '')
-
-print(magic[1::2])
-
