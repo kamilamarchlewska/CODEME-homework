a1bc1f5c6355984754087983049f2cfbeb21d151
kamilamarchlewska <61703747+kamilamarchlewska@users.noreply.github.com>
2020-04-12 00:24:39 +0200

    Delete Instrukcje sterujące 1.py

--- Instrukcje sterujące 1.py
+++
@@ -1,8 +0,0 @@
-numb = float(input('Proszę opodanie liczby: '))
-
-numb = numb % 3
-
-if numb == 0:
-    print('Twoja liczba jest podzielna przez 3.')
-else:
-    print('Twoja liczba nie jest podzielna przez 3.')
