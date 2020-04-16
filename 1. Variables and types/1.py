24de64aa5eb08758b9894fbb25dadf5d1e6ec2f9
kamilamarchlewska <61703747+kamilamarchlewska@users.noreply.github.com>
2020-04-16 03:43:11 +0200

    Variables and types
    Variables and types

---
+++ 1. Variables and types/1.py
@@ -0,0 +1,9 @@
+# Zadanie 1
+print("---------------- Ile minut ma rok? ----------------")
+num_of_weeks = 7
+num_of_days = 7
+num_of_hours = 24
+num_of_minutes = 60
+
+score = num_of_weeks * num_of_days * num_of_hours * num_of_minutes
+print('Liczba minut w ciągu 7 tygodni wynosi: ', score, 'minut')
